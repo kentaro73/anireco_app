dc8287c16c4d87b0e0a1305b98bc6e567fe04ab7
kentaro73 <k.uneune0703@gmail.com>
2021-03-30 09:14:34 +0900

    add active storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_params, only: [:show, :edit]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -12,13 +12,36 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.create(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "#{@post.title} posted successfully."
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path, notice: "{@post.title} updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: "#{@post.title} deleted successfully."
   end
 
   private
 
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :image)
     end
 
     def find_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true
+  has_one_attached :image           # active_storage
+  
 end
