c72ea785f7a038711a64d8a2b9c49db0884a0fb3
kentaro73 <k.uneune0703@gmail.com>
2021-04-05 11:04:06 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n|
+  name = Faker::Name.name
+  email = "test-#{n+1}@example.com"
+  password = "password"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
+  end
+  
+  User.create!(name: "佐藤健",
+    email: "general@example.com",
+    password: "foobaz",
+    password_confirmation: "foobaz")
+    
+  User.create!(name: "管理ユーザー",
+    email: "k.uneune0703@gmail.com",
+  admin: true,
+  password: "pmhkiy",
+  password_confirmation: "pmhkiy"
+  )
+
+users = User.order(:created_at).take(6)
+10.times do 
+# アニメタイトル
+title = Faker::Movie.title
+
+users.each {|user| user.posts.create!(title: title) }
+end
